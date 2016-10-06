39bdc86e0b47f0c876deb8fadea118c322105c7b
Shaun Anderson <sanderson@Shaun-Anderson-MacBook-pro.local>
2016-10-06 11:09:32 -0600

    Initial commit

---
+++ src/main/java/com/swiftbird/AspectBootApplication.java
@@ -0,0 +1,12 @@
+package com.swiftbird;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AspectBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AspectBootApplication.class, args);
+	}
+}
---
+++ src/test/java/com/swiftbird/AspectBootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.swiftbird;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AspectBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
